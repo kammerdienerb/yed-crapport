333f0546e0df2d94d1a41580b0855d8faf2dc2b8
Brandon Kammerdiener <kammerdienerb@MacBook-Pro-330.local>
2024-07-03 14:09:17 -0400

    pcre2

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-# gcc -o crapport.so crapport.c $(yed --print-cflags --print-ldflags) -g -O0 -Wall -Wextra -Wno-null-pointer-subtraction -Wno-gnu-null-pointer-arithmetic
-gcc -o crapport.so crapport.c $(yed --print-cflags --print-ldflags) -g -O3 -Wno-null-pointer-subtraction -Wno-gnu-null-pointer-arithmetic
+PCRE2_FLAGS=""
+if which pcre2-config > /dev/null; then
+    PCRE2_FLAGS="$(pcre2-config --cflags-posix --libs-posix) -DYED_SYNTAX_USE_PCRE2"
+fi
+
+gcc -o crapport.so crapport.c $(yed --print-cflags --print-ldflags) ${PCRE2_FLAGS} -g -O3 -Wno-null-pointer-subtraction -Wno-gnu-null-pointer-arithmetic
